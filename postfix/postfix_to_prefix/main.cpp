f116cec05ca8b426fd957c8007a6f48c9c1f434a
Immu <shaikimam37302@gmail.com>
2026-04-27 09:27:37 +0530

    Postfix to prefix Conversion

---
+++ postfix/postfix_to_prefix/main.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node{
+public:
+    string data;
+    Node *addr;
+
+    Node(string val){
+        data=val;
+        addr=nullptr;
+    }
+};
+
+class Stack{
+public:
+    Node *top;
+
+    Stack(){
+        top=nullptr;
+    }
+
+    void push(string val){
+        Node *newNode=new Node(val);
+        newNode->addr=top;
+        top=newNode;
+    }
+
+    string pop(){
+        if(top==nullptr)
+            return "";
+
+        string val=top->data;
+        top=top->addr;
+        return val;
+    }
+
+    bool isOperator(char ch){
+        return (ch=='+'||ch=='-'||ch=='*'||ch=='/');
+    }
+
+    void postfixTOPrefix(string postfix){
+
+        for(int i=0;i<postfix.length();i++){
+
+            char ch=postfix[i];
+
+            if(isOperator(ch)){
+
+                string op2=pop();
+                string op1=pop();
+
+                string temp=ch+op1+op2;
+                push(temp);
+            }
+
+            else{
+                push(string(1,ch));
+            }
+        }
+
+        cout<<pop();
+    }
+};
+
+int main(){
+
+    string postfix;
+    cin>>postfix;
+
+    Stack s;
+    s.postfixTOPrefix(postfix);
+
+    return 0;
+}
